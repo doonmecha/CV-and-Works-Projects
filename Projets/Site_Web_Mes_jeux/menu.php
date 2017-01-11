7f70358a0cd8f2bb721cefdc34e7f91662147548
Doon <petersteele@hotmail.fr>
2017-01-11 17:07:19 +0100

    Begin php migration

---
+++ Projets/Site_Web_Mes_jeux/menu.php
@@ -0,0 +1,14 @@
+ <nav>
+            <!-- top nav -->
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Nos jeux</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Nos guides</a></li>
+                    <li><a href="#">Forum</a></li>
+                    <li><a href="#">Nous contacter</a></li>
+                    <li><a href="#">L'équipe</a></li>
+                </ul>
+            </div>
+        </nav>
