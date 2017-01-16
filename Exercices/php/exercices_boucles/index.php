6821c67a99377836b443444e68bc3330b98d7a20
Doon <petersteele@hotmail.fr>
2017-01-16 13:34:53 +0100

    Update exo boucle

---
+++ Exercices/php/exercices_boucles/index.php
@@ -0,0 +1,23 @@
+<?php
+            $nombre = 1;
+
+
+            while ($nombre < 11)
+            {
+                echo  "$nombre*o*<br>";
+                $nombre++; 
+            }
+
+                if ($nombre = 10)
+                $nombre++;
+                    echo "FIN :D";
+                    echo "o_o<br>";
+
+            for ($nombre=10; $nombre<300; $nombre++){
+                
+                    echo "$nombre BUG<br>"; 
+                }
+
+
+
+                ?>
